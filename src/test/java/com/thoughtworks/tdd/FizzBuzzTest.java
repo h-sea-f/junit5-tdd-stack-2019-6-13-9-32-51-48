b478d3c5a092915998c79e09946d55d8f26aa11b
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-12 16:04:40 +0800

    amy ouyang : finish should return Buzz given 10, test

--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -60,4 +60,15 @@ public class FizzBuzzTest {
         //Then
         assertEquals(result,"Fizz");
     }
+
+    @Test
+    @DisplayName("should return Buzz when convert given 10")
+    public void should_return_Buzz_when_convert_given_10(){
+        //Given
+        int number=10;
+        //when
+        String result=new FizzBuzz().convert(number);
+        //Then
+        assertEquals(result,"Buzz");
+    }
 }

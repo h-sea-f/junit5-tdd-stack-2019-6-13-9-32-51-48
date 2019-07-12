4e5c62a2789dce5f1c6e8890099187ab93d3541f
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-12 16:02:41 +0800

    amy ouyang : finish should return Fizz given 6, implement

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -2,7 +2,7 @@ package com.thoughtworks.tdd;
 
 public class FizzBuzz {
     public String convert(int number) {
-        if (number == 3) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
 
